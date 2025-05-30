af3e627cdcdc578c66cc2e29ae50166f7d47ca7f
Arthur Vitório Sbeghen <arthursbeghenn@outlook.com>
2025-05-29 23:00:12 -0300

    Comenta endpoints

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\FichaController;
 use App\Http\Controllers\MesaController;
 use App\Http\Controllers\UserController;
@@ -9,33 +8,33 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('tables')->group(function () {
-    Route::get('/', [MesaController::class, 'index']);
-    Route::get('/{id}', [MesaController::class, 'show']);
-    Route::post('/', [MesaController::class, 'store']);
-    Route::put('/{id}', [MesaController::class, 'update']);
-    Route::delete('/{id}', [MesaController::class, 'destroy']);
+    Route::get('/', [MesaController::class, 'index']); // listagem
+    Route::get('/{id}', [MesaController::class, 'show']); // Listagem específica
+    Route::post('/', [MesaController::class, 'store']); // Adiciona registro
+    Route::put('/{id}', [MesaController::class, 'update']); // Altera registro
+    Route::delete('/{id}', [MesaController::class, 'destroy']); // "Apaga" registro (softdelete)
 });
 
 Route::prefix('auth')->group(function () {
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
-    Route::post('/reset-password', [AuthController::class, 'resetPassword']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::post('/data', [AuthController::class, 'data']);
+    Route::post('/login', [AuthController::class, 'login']); // Loga usuário
+    Route::post('/register', [AuthController::class, 'register']); // Adiciona registro de autenticação
+    Route::post('/forgot-password', [AuthController::class, 'forgotPassword']); // Encaminha instruções para recuperação do registro
+    Route::post('/reset-password', [AuthController::class, 'resetPassword']); // Altera a senha do registro
+    Route::post('/logout', [AuthController::class, 'logout']); // Desloga usuário
+    Route::post('/data', [AuthController::class, 'data']); // Mostra dados da autenticação (token)
 });
 
 Route::prefix('users')->group(function () {
-    Route::get('/{id}', [UserController::class, 'show']);
-    Route::post('/create', [UserController::class, 'store']);
-    Route::put('/{id}', [UserController::class, 'update']);
-    Route::delete('/{id}', [UserController::class, 'destroy']);
+    Route::get('/{id}', [UserController::class, 'show']); // Mostra dados do usuário
+    Route::post('/create', [UserController::class, 'store']); // Adiciona registro
+    Route::put('/{id}', [UserController::class, 'update']); // Altera dados básicos do registro
+    Route::delete('/{id}', [UserController::class, 'destroy']); // Apaga registro
 });
 
 Route::prefix('fichas')->group(function () {
-    Route::get('/', [FichaController::class, 'index']);
-    Route::get('/{id}', [FichaController::class, 'show']);
-    Route::post('/', [FichaController::class, 'store']);
-    Route::put('/{id}', [FichaController::class, 'update']);
-    Route::delete('/{id}', [FichaController::class, 'destroy']);
+    Route::get('/', [FichaController::class, 'index']); // listagem
+    Route::get('/{id}', [FichaController::class, 'show']); // Listagem específica
+    Route::post('/', [FichaController::class, 'store']); // Adiciona registro
+    Route::put('/{id}', [FichaController::class, 'update']); // Altera registro
+    Route::delete('/{id}', [FichaController::class, 'destroy']); // "Apaga" registro (softdelete)
 });
