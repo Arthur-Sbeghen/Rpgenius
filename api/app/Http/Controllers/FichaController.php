3c6cf4460e55aee84013074a32934db645a029d7
R0moo <125042231+R0moo@users.noreply.github.com>
2025-06-10 22:10:31 -0300

    Seeders & Includes (start)
    Realizado o começo dos seeders (Tables e Systems) e dos includes. Além da correção da tabela tables (adicionado timestamps ao invés de current date)

--- api/app/Http/Controllers/FichaController.php
+++ api/app/Http/Controllers/FichaController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 class FichaController extends Controller
 {
     public function index () {}
+    public function create(){}
+    public function edit(){}
     public function show ($id) {}
     public function store (Request $request) {}
     public function update (Request $request, $id) {}
