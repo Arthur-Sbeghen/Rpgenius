8ee951ea6a80b5b8065ab6583e2425c50313b30c
ENRICO PAROLIN <enrico.311007@bento.local>
2025-06-27 11:40:07 -0300

    componentes car e botao

---
+++ app/src/app/teste/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { Button } from "@/components/Button/Button";
+import { CardTable } from "@/components/CardTable/CardTable";
+
+
+export default function AboutPage() {
+  
+  return (
+    <>
+        <Button type="success" text="opa"/>
+        <Button type="default" text="opa"/>
+        <CardTable title="opa" image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU-quPbAQMD3Ab9hegEbrrJBBAgfoW0aGIbw&s" system="ded" creator="ze"/>
+        <CardTable title="opa" image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJ5jUsQfDuHioH8QLly2AK9e0M3yksbkl4-g&s" system="ded" creator="ze"/>
+    </>
+  );
+}
