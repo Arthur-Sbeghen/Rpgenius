26599a2b63e6829443d9a62b1f3f144d58d60ab2
Arthur Vitório Sbeghen <arthursbeghenn@outlook.com>
2025-06-27 09:28:33 -0300

    Revert "Reapply "Merge branch 'main' into create-enter-table""
    This reverts commit 9b5451f27d688338166a7610c1ddf0a52130b216.

--- app/src/lib/auth.ts
+++ app/src/lib/auth.ts
@@ -2,27 +2,22 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-
 import Cookies from "js-cookie";
 
-export function authCheck({ requireAuth = false }: { requireAuth: boolean }) {
+export function useCookie({ requireAuth = false }: { requireAuth: boolean }) {
   const router = useRouter();
   const [checked, setChecked] = useState(false);
   const [allowed, setAllowed] = useState(false);
-
   useEffect(() => {
     const token = Cookies.get("authToken");
-
     if (requireAuth && !token) {
       router.replace("/auth");
       return;
     }
-
     if (!requireAuth && token) {
       router.replace("/table");
       return;
     }
-
     setAllowed(true);
     setChecked(true);
   }, [requireAuth, router]);
